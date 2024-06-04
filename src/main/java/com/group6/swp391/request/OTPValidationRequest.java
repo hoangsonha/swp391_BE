c8651afdf296a381457218c5530d134f6fd39535
Hoang Son Ha <hoangsonhadev@gmail.com>
2024-06-04 21:42:36 +0700

    Sơn Hà [ADD] ADD function forget password by send SMS and send Email when user input with email or password and then input 6 number to reset password

--- src/main/java/com/group6/swp391/request/OTPValidationRequest.java
+++ src/main/java/com/group6/swp391/request/OTPValidationRequest.java
@@ -10,6 +10,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OTPValidationRequest {
-    private String phone;
+    private String phoneOrEmail;
     private String otp;
 }
