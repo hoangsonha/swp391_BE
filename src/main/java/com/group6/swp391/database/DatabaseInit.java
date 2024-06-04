edc0d77a160cb3033976292f0e140534d803da3e
Hoang Son Ha <hoangsonhadev@gmail.com>
2024-06-04 20:10:46 +0700

    Sơn Hà [UPDATE] fix bug function login

--- src/main/java/com/group6/swp391/database/DatabaseInit.java
+++ src/main/java/com/group6/swp391/database/DatabaseInit.java
@@ -25,7 +25,7 @@ public class DatabaseInit {
         return new CommandLineRunner() {
             @Override
             public void run(String... args) throws Exception {
-                Role role_admin = new Role(EnumRoleName.ROLE_ADMIN, null);
+//                Role role_admin = new Role(EnumRoleName.ROLE_ADMIN, null);
 //                Role role_staff = new Role(EnumRoleName.ROLE_STAFF, null);
 //                Role role_user = new Role(EnumRoleName.ROLE_USER, null);
 //                Role role_delivery = new Role(EnumRoleName.ROLE_DELIVERY, null);
@@ -37,8 +37,8 @@ public class DatabaseInit {
 //
 //                Role role = roleRepository.getRoleByRoleName(EnumRoleName.ROLE_ADMIN);
 //
-//                User user = new User("Hoang", "Son", "hahsse16042922@gmail.com", bCryptPasswordEncoder.encode("1"), "0334386995", "Lê Văn Việt", "itachi.jpg", "abcxyz", true, true, role);
-
+//                User user = new User("Hoang", "Son", "hahsse16042922@gmail.com", bCryptPasswordEncoder.encode("1"), "0334386995", "Lê Văn Việt", "itachi.jpg", "abcxyz", true, true, role, 0 , null);
+//
 //                userRepository.save(user);
             }
         };
