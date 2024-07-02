84dcc077c8d2a72778aaf46a6e7eb2099ebe80c8
hoangsonha <hahsse160429@fpt.edu.vn>
2024-07-02 20:28:16 +0700

    Sơn Hà [UPDATE] bug

--- src/main/java/com/group6/swp391/database/DatabaseInit.java
+++ src/main/java/com/group6/swp391/database/DatabaseInit.java
@@ -9,7 +9,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class DatabaseInit {
 
     @Autowired private UserRepository userRepository;
