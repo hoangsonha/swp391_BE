cab52c8eca95afa8ca2eed6919ab234a5d089c4e
Hiep <doanmanhhiep.2003@gmail.com>
2024-07-04 14:14:52 +0700

    hiepdm - fix date format BaseEntity

--- src/main/java/com/group6/swp391/model/BaseEntity.java
+++ src/main/java/com/group6/swp391/model/BaseEntity.java
@@ -7,7 +7,8 @@ import jakarta.persistence.PreUpdate;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @MappedSuperclass
 @Getter
@@ -17,20 +18,22 @@ import java.time.LocalDateTime;
 public class BaseEntity {
     @Column(name = "create_at")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDateTime createAt;
+    private ZonedDateTime createAt;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "update_at")
-    private LocalDateTime updateAt;
+    private ZonedDateTime updateAt;
 
     @PrePersist
     protected void onCreate() {
-        createAt = LocalDateTime.now();
-        updateAt = LocalDateTime.now();
+        ZoneId zoneId = ZoneId.of("Asia/Ho_Chi_Minh");
+        createAt = ZonedDateTime.now(zoneId);
+        updateAt = ZonedDateTime.now(zoneId);
     }
 
     @PreUpdate
     protected void onUpdate() {
-        updateAt = LocalDateTime.now();
+        ZoneId zoneId = ZoneId.of("Asia/Ho_Chi_Minh");
+        updateAt = ZonedDateTime.now(zoneId);
     }
 }
